37d2926d8a2834b338188207328eaa7bc4f28602
Ding Lyu <lyudingguangnz@outlook.com>
2022-05-11 22:15:30 +1200

    Doc created

---
+++ app.js
@@ -0,0 +1,10 @@
+// let arr = ["Harry", "Ding", "Condordia"];
+
+// for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+//   console.log(i);
+// }
+
+// arr.forEach((person) => {
+//   console.log(person);
+// });
